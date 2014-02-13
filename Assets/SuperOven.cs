2721509c21abcdba07c109acf69079ad88a2afd9
Mohamed Chehab <chehab.mohamed@gmail.com>
2014-02-13 18:35:08 -0500

    Added basic objects, thermometer, player, burner
    - Basic player interactions
    - Mouse can control the player
    - Moving into the box increases temperature

---
+++ Assets/SuperOven.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperOven : MonoBehaviour {
+
+	public GameObject player;
+	public bool test = false;
+	public GameObject thermometer;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		thermometer = GameObject.Find ("Thermometer");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "Player") {
+			test = true;
+			Thermometer.temp++;
+		}
+	}
+
+
+//	void OnCollisionEnter(Collision other) {
+//		
+//		if (other.gameObject.tag == "Player") {
+//			test = true;
+//			Destroy (other.gameObject);
+//		}
+//	}
+
+}
---
+++ Assets/Thermometer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thermometer : MonoBehaviour {
+
+	public static float temp = 25;
+	TextMesh tm;
+
+	// Use this for initialization
+	void Start () {
+		tm = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tm.text = temp+"";		
+	}
+}
