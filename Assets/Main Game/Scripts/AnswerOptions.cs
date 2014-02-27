b74747255d7efa8b4712c516f37dbc1ff31b311d
Henry Chung <Henry.Chung@Desire2Learn.com>
2014-02-27 18:08:26 -0500

    Added answers bar. Replaced FollowMouse from .js to .cs. Mystery compound stops following cursor on click.

---
+++ Assets/Main Game/Scripts/AnswerOptions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerOptions : MonoBehaviour {
+
+	private int buttonHeight = 30;
+	private int buttonWidth = 100;
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (0, 0, buttonWidth, buttonHeight), "Correct 1")) {
+			Application.LoadLevel("Results");
+		}
+		if (GUI.Button (new Rect (buttonWidth, 0, buttonWidth, buttonHeight), "Correct 2")) {
+			Application.LoadLevel("Results");
+		}
+		if (GUI.Button (new Rect (buttonWidth * 2, 0, buttonWidth, buttonHeight), "Wrong 1")) {
+			Application.LoadLevel("Results");
+		}
+		if (GUI.Button (new Rect (buttonWidth * 3, 0, buttonWidth, buttonHeight), "Wrong 2")) {
+			Application.LoadLevel("Results");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Main Game/Scripts/FollowMouse.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+
+	public bool followMouse = true;
+	private float depth = 10.0f;
+
+	void OnGUI() {
+		if (Event.current.type == EventType.MouseDown) {
+			followMouse = !followMouse;
+			Screen.showCursor = !followMouse;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!followMouse) {
+			return;	
+		}
+		Vector3 mousePositionVector = Input.mousePosition;
+		Vector3 wantedPosition = Camera.main.ScreenToWorldPoint(
+			new Vector3(
+				mousePositionVector.x, 
+				mousePositionVector.y, 
+				depth)
+			);
+		transform.position = wantedPosition;
+	}
+}
