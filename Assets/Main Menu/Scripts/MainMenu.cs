79332bb7e396606ecc23a85650a53f4a50a1dfd5
Henry Chung <Henry.Chung@Desire2Learn.com>
2014-02-27 17:45:24 -0500

    Added main menu. Restructured project hierarchy.

---
+++ Assets/Main Menu/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (150,100,300,75), "Start Game")) {
+			print ("You clicked the button!");
+			Application.LoadLevel("Main Game");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
