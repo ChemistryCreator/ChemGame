8dd9890cafd84f4c326c179b6514dd111397a521
mallika <mallika.trivedi3@gmail.com>
2014-03-03 22:08:09 -0500

    Added results screen with button leading back to main game

---
+++ Assets/Results Panel/Scripts/PlayAgain.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAgain : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (150,100,300,75), "Play Again")) {
+			print ("You clicked the button!");
+			Application.LoadLevel("Main Game");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
